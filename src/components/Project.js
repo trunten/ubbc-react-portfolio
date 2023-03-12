cfc945e83232ad76b6cefb8051167dc43d82be8c
Jonathan Davis <trunten@btinternet.com>
2023-03-12 18:23:49 +0000

    moved images, updated project styling and click handling

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,32 @@
-import { useState } from "react";
-import placeholder from "../images/placeholder.jpg";
+// import { useState } from "react";
+// import placeholder from "../images/placeholder.jpg";
+
 export default function Project(props) {  
   const {title, languages, description, image, link, gh, hero} = props.project;
-  const [img, setImg] = useState(placeholder);
   
-  function getImg(src) {
-    import(`../images/${src}`).then((image) => setImg(image.default));
+  // Dynamic imports from src folder - works but needs more testing
+  // const [img, setImg] = useState(placeholder);
+  // function getImg(src) {
+  //   import(`../images/${src}`).then((image) => setImg(image.default));
+  // }
+  // getImg(image.replace("/images/",""));
+  
+  function cardClick(e) {
+    if (e.target.tagName !== "A") {
+      window.open(link || gh, "_blank");
+    }
   }
-  
-  getImg(image.replace("/images/",""));
-  
+
   return (
-    <div className={"mb-4 " + (hero ? "col-sm-12 col-md-8 col-lg-6" : "col-sm-6 col-md-4 col-lg-3")} onClick={() => document.getElementById(props.pid).click()}>
+    <div className={"mb-4 " + (hero ? "col-sm-12 col-md-8 col-lg-6" : "col-sm-6 col-md-4 col-lg-3")} onClick={cardClick}>
       <div className="card h-100">
         <div className="img-wrapper">
           <div className="buttons">
-            {link && <a id={link ? props.pid : ""} href={link} target="_blank" className={`btn btn-outline-light rounded-pill ${gh ? "mb-3" : ""}`} rel="noreferrer">Deployed Site</a>}
-            {gh && <a id={gh ? props.pid : ""} href={gh} target="_blank" className={`btn btn-outline-light rounded-pill ${link ? "mt-3" : ""}`} rel="noreferrer">GitHub Repo</a>}
+            {link && <a href={link} target="_blank" className={`btn btn-outline-light rounded-pill ${gh ? "mb-3" : ""}`} rel="noreferrer">Deployed Site</a>}
+            {gh && <a href={gh} target="_blank" className={`btn btn-outline-light rounded-pill ${link ? "mt-3" : ""}`} rel="noreferrer">GitHub Repo</a>}
           </div>
-          <img src={img} className="card-img-top" loading="lazy" alt={description || "project screenshot"} />
-          {/* <img src={process.env.PUBLIC_URL + image} className="card-img-top" loading="lazy" alt={description || "project screenshot"} /> */}
+          {/* <img src={img} className="card-img-top" alt={description || "project image"} /> */}
+          <img src={process.env.PUBLIC_URL + image} className="card-img-top" loading="lazy" alt={description || "project image"} />
         </div>
         <div className="card-body px-3 pb-0" style={{position: "relative"}}>
           <h5 className="card-title my-0">{title}</h5>
